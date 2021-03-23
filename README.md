88ece0ce840ab7f614433944c93b3e9fd0c18857
Nasir Osman <nmosman@ucalgary.ca>
2021-03-23 00:21:02 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+WatchOS Haptics Test
---
+++ watchOS-haptics WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,70 @@
+//
+//  InterfaceController.swift
+//  watchOS-haptics WatchKit Extension
+//
+//  Adopted from yorifuji on 2020/05/09.
+//
+//
+
+import WatchKit
+import Foundation
+
+class MyRowController: NSObject {
+    @IBOutlet weak var itemLabel: WKInterfaceLabel!
+}
+
+class InterfaceController: WKInterfaceController {
+
+    @IBOutlet weak var table: WKInterfaceTable!
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+        loadItems()
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+    override func table(_ table: WKInterfaceTable, didSelectRowAt rowIndex: Int) {
+        print(#function)
+        print(rowIndex)
+        if let hapticType = WKHapticType(rawValue: rowIndex) {
+            for _ in 1...5{
+                WKInterfaceDevice.current().play(hapticType)
+            }
+            
+        }
+     }
+}
+
+extension InterfaceController {
+    func loadItems() {
+        let items = [
+            "notification",
+            "directionUp",
+            "directionDown",
+            "success",
+            "failure",
+            "retry",
+            "start",
+            "stop",
+            "click"
+        ]
+
+        table.setNumberOfRows(items.count, withRowType: "MyRowController")
+
+        for (index, item) in items.enumerated() {
+            let row = table.rowController(at: index) as! MyRowController
+            row.itemLabel.setText(item)
+        }
+    }
+}
